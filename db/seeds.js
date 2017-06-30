69962928b86af6831a9dee48fa02c5a94e9a04b5
Jakub Dziekan <jdziek89@gmail.com>
2017-06-30 16:13:48 +0100

    that bastards working

--- db/seeds.js
+++ db/seeds.js
@@ -9,75 +9,93 @@ Student.collection.drop();
 const studentData = [{
   name: 'Lauren Armbrust',
   image: 'images/lauren.jpg',
-  gitHub: 'https://github.com/larmflower'
+  gitHub: 'https://github.com/larmflower',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Simon Amor',
   image: 'images/simon.jpg',
-  gitHub: 'https://github.com/simonrramor'
+  gitHub: 'https://github.com/simonrramor',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Nick Cresner',
   image: 'images/nick.jpg',
-  gitHub: 'https://github.com/nickcresner'
+  gitHub: 'https://github.com/nickcresner',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Alex Okamoto',
   image: 'images/alexo.jpg',
-  gitHub: 'https://github.com/alexandriako'
+  gitHub: 'https://github.com/alexandriako',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Jim Harris',
   image: 'images/jim.jpg',
-  gitHub: 'https://github.com/jgbharris'
+  gitHub: 'https://github.com/jgbharris',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Angela Maugey',
   image: 'images/angela.jpg',
-  gitHub: 'https://github.com/maugeya'
+  gitHub: 'https://github.com/maugeya',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Alex Cwiek',
   image: 'images/alexc.jpg',
-  gitHub: 'https://github.com/alexcviek'
+  gitHub: 'https://github.com/alexcviek',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Jack May',
   image: 'images/jack.jpg',
-  gitHub: 'https://github.com/Jack11709'
+  gitHub: 'https://github.com/Jack11709',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'James Clarke',
   image: 'images/james.jpg',
-  gitHub: 'https://github.com/JamesRClarke'
+  gitHub: 'https://github.com/JamesRClarke',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Olly Middleton',
   image: 'images/olly.jpg',
-  gitHub: 'https://github.com/Ollymid'
+  gitHub: 'https://github.com/Ollymid',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Tim Rooke',
   image: 'images/tim.jpg',
-  gitHub: 'https://github.com/timrooke1991'
+  gitHub: 'https://github.com/timrooke1991',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Nimesh Patel',
   image: 'images/nimesh.jpg',
-  gitHub: 'https://github.com/n2kp'
+  gitHub: 'https://github.com/n2kp',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Mike de Groot',
   image: 'images/mike.jpg',
-  gitHub: 'https://github.com/mikejdegroot'
+  gitHub: 'https://github.com/mikejdegroot',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Jakub Dziekan',
   image: 'images/jakub.jpg',
-  gitHub: 'https://github.com/Foxford13'
+  gitHub: 'https://github.com/Foxford13',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Clare McLoughlin ',
   image: 'images/clare.jpg',
-  gitHub: 'https://github.com/McLoughlinClare'
+  gitHub: 'https://github.com/McLoughlinClare',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Jason Ranjitkumar',
   image: 'images/jason.jpg',
-  gitHub: 'https://github.com/jasontrk'
+  gitHub: 'https://github.com/jasontrk',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Chisomo Lungu',
   image: 'images/chisomo.jpg',
-  gitHub: 'https://github.com/obfusticatedcode'
+  gitHub: 'https://github.com/obfusticatedcode',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }, {
   name: 'Max Eskell',
   image: 'images/max.jpg',
-  gitHub: 'https://github.com/maxeskell'
+  gitHub: 'https://github.com/maxeskell',
+  location: { lat: 51.5221596, lng: -0.0521308 }
 }];
 
 Student
--- models/student.js
+++ models/student.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 
+
 const studentSchema = new mongoose.Schema({
   name: { type: String },
   image: { type: String },
-  gitHub: { type: String }
+  gitHub: { type: String },
+  location: { lat: Number, lng: Number }
+},{
+  timestamps: true
 });
+
+
 
 
 module.exports = mongoose.model('Student', studentSchema);
